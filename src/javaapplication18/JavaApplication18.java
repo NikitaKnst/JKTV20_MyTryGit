1540996415dfc0236879f08d529dd0b70c7a17aa
Alebro <pupil@ivkhk.ee>
2021-09-06 08:45:19 +0300

    MyThreeTryGit

--- src/javaapplication18/JavaApplication18.java
+++ src/javaapplication18/JavaApplication18.java
@@ -17,6 +17,8 @@ public class JavaApplication18 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("MyFirstTryGit");
+                System.out.println("MySecondTryGit");
+
     }
     
 }
